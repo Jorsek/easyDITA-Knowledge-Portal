4d92c6d7828506c36d0b689f906c86f493e4a68c
Scott Lucchini <scott.lucchini@easydita.com>
2015-10-07 15:43:13 -0400

    Added google analytics tracking to wp_head.

--- easydocs/functions.php
+++ easydocs/functions.php
@@ -165,6 +165,23 @@ function get_the_shortdesc() {
 }
 
 /**
+ * Add Google Analytics stuff to wp_head
+**/
+function insert_ga_info() {
+	echo "<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', 'UA-68186023-1', 'auto');
+		  ga('send', 'pageview');
+		
+		</script>";
+}
+add_action('wp_head','insert_ga_info');
+
+/**
  * Add JQuery
  **/
 wp_enqueue_script("jquery");
