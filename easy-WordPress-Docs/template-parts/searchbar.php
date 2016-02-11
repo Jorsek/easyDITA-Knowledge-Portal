0534685af2bc6e215045763c04f3d3b0222b2d56
Scott Lucchini <scott.lucchini@easydita.com>
2016-02-11 16:56:45 -0500

    Added search button (hidden in default skin) to the search results page. Reclassed the X button to clear the search.

--- easy-WordPress-Docs/template-parts/searchbar.php
+++ easy-WordPress-Docs/template-parts/searchbar.php
@@ -13,11 +13,12 @@
 
 <?php else : ?>
 
-	<div class="small-search">
+	<div class="small-search <?php if (is_search()) { echo 'is-search'; } ?>">
 	  <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
         <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo get_theme_mod( 'search_placeholder', 'Have a question? Ask or enter a search term.' ); ?>" />
         <?php if (is_search()) : ?>
-			<a class="submit" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"><i class="fa fa-times"></i></a>
+			<a class="clear-icon" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"><i class="fa fa-times"></i></a>
+			<button type="submit" class="submit" name="submit" id="searchsubmit"><i class="fa fa-search"></i><span class="text">Search</span></button>
 		<?php else : ?>
 			<button type="submit" class="submit" name="submit" id="searchsubmit"><i class="fa fa-search"></i><span class="text">Search</span></button>
 		<?php endif; ?>
