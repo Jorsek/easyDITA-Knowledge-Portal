c34d8d9c71ea1816778df90d74096ddc022cce2d
Scott Lucchini <scott.lucchini@easydita.com>
2016-03-03 13:08:17 -0500

    Structure and styling for comments.

--- easy-WordPress-Docs/comments.php
+++ easy-WordPress-Docs/comments.php
@@ -7,7 +7,7 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package _s
+ * @package easy_wordpress_docs
  */
 
 /*
@@ -17,6 +17,76 @@
  */
 if ( post_password_required() ) {
 	return;
+}
+
+class Comment_Walker extends Walker {
+	var $db_fields = array(
+		'parent' => 'comment_parent',
+		'id' => 'comment_ID'
+	);
+	
+	// start_lvl - wrapper for child comments list
+	function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$GLOBALS['comment_depth'] = $depth + 2; ?>
+		
+		<section class="child-comments comments-list">
+
+	<?php }
+
+	// end_lvl - closing wrapper for child comments list
+	function end_lvl( &$output, $depth = 0, $args = array() ) {
+		$GLOBALS['comment_depth'] = $depth + 2; ?>
+
+		</section>
+
+	<?php }
+
+	// start_el - HTML for comment template
+	function start_el( &$output, $comment, $depth = 0, $args = array(), $id = 0 ) {
+		$depth++;
+		$GLOBALS['comment_depth'] = $depth;
+		$GLOBALS['comment'] = $comment;
+		$parent_class = ( empty( $args['has_children'] ) ? '' : 'parent' ); 
+
+		if ( 'article' == $args['style'] ) {
+			$tag = 'article';
+			$add_below = 'comment';
+		} else {
+			$tag = 'article';
+			$add_below = 'comment';
+		} ?>
+
+		<article <?php comment_class(empty( $args['has_children'] ) ? '' :'parent') ?> id="comment-<?php comment_ID() ?>" itemprop="comment" itemscope itemtype="http://schema.org/Comment">
+			<div class="comment-header">
+				<figure class="gravatar"><?php echo get_avatar($comment, 32); ?></figure>
+				<div class="comment-meta post-meta" role="complementary">
+					<div class="comment-author"><?php comment_author(); ?></div>
+					<time class="comment-time" datetime="<?php comment_date('Y-m-d') ?>T<?php comment_time('H:iP') ?>" itemprop="datePublished"><?php comment_date('jS F Y') ?>, <a href="#comment-<?php comment_ID() ?>" itemprop="url"><?php comment_time() ?></a></time>
+					<div class="edit-link">
+						<?php edit_comment_link('Edit this comment','',''); ?>
+					</div>
+					<?php if ($comment->comment_approved == '0') :
+						echo 'Your comment is awaiting moderation.';
+					endif; ?>
+				</div>
+			</div>
+			<div class="comment-content post-content" itemprop="text">
+				<?php comment_text() ?>
+				<div class="comment-reply-area">
+					<i class="fa fa-reply fa-flip-horizontal"><!-- filler --></i>
+					<?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+				</div>
+			</div>
+
+	<?php }
+
+	// end_el - closing HTML for comment template
+	function end_el(&$output, $comment, $depth = 0, $args = array() ) { ?>
+
+		</article>
+
+	<?php }
+	
 }
 ?>
 
@@ -47,14 +117,13 @@ if ( post_password_required() ) {
 		</nav><!-- #comment-nav-above -->
 		<?php endif; // Check for comment navigation. ?>
 
-		<ol class="comment-list">
+		<div class="comment-list">
 			<?php
 				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
+					'walker' => new Comment_Walker(),
 				) );
 			?>
-		</ol><!-- .comment-list -->
+		</div><!-- .comment-list -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
@@ -77,6 +146,31 @@ if ( post_password_required() ) {
 		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', '_s' ); ?></p>
 	<?php endif; ?>
 
-	<?php comment_form(); ?>
+	<?php
+	$fields = array(
+			'author' =>
+				'<div class="comment-form-extra comment-form-author"><label for="author">' . __( 'Name', 'easy_wordpress_docs' ) . '</label> ' .
+				( $req ? '<span class="required">*</span>' : '' ) .
+				'<input id="author" name="author" type="text" value="' . esc_attr($commenter['comment_author']) .
+				'" size="30"' . $aria_req . ' /></div>',
+			
+			'email' =>
+				'<div class="comment-form-extra comment-form-email"><label for="email">' . __( 'Email', 'easy_wordpress_docs' ) . '</label> ' .
+				( $req ? '<span class="required">*</span>' : '' ) .
+				'<input id="email" name="email" type="text" value="' . esc_attr($commenter['comment_author_email']) .
+				'" size="30"' . $aria_req . ' /></div>',
+			
+			'breaker' =>
+				'<div class="breaker"><!-- filler --></div>'
+		);
+	
+	comment_form(array(
+			'fields' => apply_filters('comment_form_default_fields', $fields),
+			'comment_field' => '<div class="comment-form-comment"><textarea id="comment" name="comment" cols="45" rows="3" aria-required="true" placeholder="Comment . . ."></textarea></div>',
+			'logged_in_as' => '<div class="logged-in-as">' . sprintf( __( '<div class="user">Logged in as <a href="%1$s">%2$s</a>.</div><div class="log-out"><a href="%3$s" title="Log out of this account">Log out?</a></div>' ), admin_url( 'profile.php' ), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) ) ) . '</div>',
+			'title_reply' => __('Leave a Comment'),
+			'title_reply_to' => __('Reply to %s')
+		));
+	?>
 
 </div><!-- #comments -->
