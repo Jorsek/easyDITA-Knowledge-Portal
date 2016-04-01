441314992a640c10c34721735acdc8a5cb51a87c
Scott Lucchini <scott.lucchini@easydita.com>
2016-04-01 14:07:47 -0400

    Footer social icons are enabled by default. If no social icons are selected to appear, the entire social block doesn't appear.

--- easydita-knowledge-portal/footer.php
+++ easydita-knowledge-portal/footer.php
@@ -15,21 +15,26 @@
 		<div class="user-text">
 	  		<?php echo preg_replace("/^(.*?)<br\/>/",'<span class="title">$1</span><br/>',preg_replace("/\s*\n\s*/","<br/>",get_theme_mod( 'footer_html', 'Copyright 2016' ))) ?>
   		</div>
-  		<div class="social">
-  			<div class="title">Social</div>
-  			<?php if (get_theme_mod('facebook_enabled') == 1) : ?>
-  				<a href="<?php echo get_theme_mod('facebook_link','#') ?>" target="_blank"><i class="facebook-icon"></i></a>
-  			<?php endif ?>
-  			<?php if (get_theme_mod('twitter_enabled') == 1) : ?>
-  				<a href="<?php echo get_theme_mod('twitter_link','#') ?>" target="_blank"><i class="twitter-icon"></i></a>
-  			<?php endif ?>
-  			<?php if (get_theme_mod('google_enabled') == 1) : ?>
-  				<a href="<?php echo get_theme_mod('google_link','#') ?>" target="_blank"><i class="google-icon"></i></a>
-  			<?php endif ?>
-  			<?php if (get_theme_mod('linkedin_enabled') == 1) : ?>
-  				<a href="<?php echo get_theme_mod('linkedin_link','#') ?>" target="_blank"><i class="linkedin-icon"></i></a>
-  			<?php endif ?>
-  		</div>
+  		<?php if (get_theme_mod('facebook_enabled',1) == 1 ||
+  				get_theme_mod('twitter_enabled',1) == 1 ||
+  				get_theme_mod('google_enabled',1) == 1 ||
+  				get_theme_mod('linkedin_enabled',1) == 1) : ?>
+	  		<div class="social">
+	  			<div class="title">Social</div>
+	  			<?php if (get_theme_mod('facebook_enabled',1) == 1) : ?>
+	  				<a href="<?php echo get_theme_mod('facebook_link','#') ?>" target="_blank"><i class="facebook-icon"></i></a>
+	  			<?php endif ?>
+	  			<?php if (get_theme_mod('twitter_enabled',1) == 1) : ?>
+	  				<a href="<?php echo get_theme_mod('twitter_link','#') ?>" target="_blank"><i class="twitter-icon"></i></a>
+	  			<?php endif ?>
+	  			<?php if (get_theme_mod('google_enabled',1) == 1) : ?>
+	  				<a href="<?php echo get_theme_mod('google_link','#') ?>" target="_blank"><i class="google-icon"></i></a>
+	  			<?php endif ?>
+	  			<?php if (get_theme_mod('linkedin_enabled',1) == 1) : ?>
+	  				<a href="<?php echo get_theme_mod('linkedin_link','#') ?>" target="_blank"><i class="linkedin-icon"></i></a>
+	  			<?php endif ?>
+	  		</div><!-- .social -->
+  		<?php endif; ?>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
